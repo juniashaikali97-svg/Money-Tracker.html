5544bd8fde21b6aff644318b34b4fbee833a3844
juniashaikali97-svg <juniashaikali97@gmail.com>
2026-08-13 06:37:43 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Simple function to handle button clicks
+document.querySelectorAll('button').forEach(button => {
+    button.addEventListener('click', () => {
+        alert("Thank you for your interest! Our application form will open shortly.");
+    });
+});
+
+// Log a message to the console to confirm the script is working
+console.log("Cashnet Finance website is ready!");
